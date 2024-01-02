b258637c19038ff049ce35bbf91d2bf37ab3f976
Russell Troxel <russell.troxel@segment.com>
2024-01-02 15:09:59 -0800

    [LYB-38] Add Tests for config library
    Signed-off-by: Russell Troxel <russell.troxel@segment.com>

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,3 +1,319 @@
 package config
 
-// TODO: Tests for config
+import (
+	"testing"
+	"time"
+
+	flag "github.com/spf13/pflag"
+	"github.com/stretchr/testify/require"
+)
+
+func testFlagSet() *flag.FlagSet {
+	flagSet := flag.NewFlagSet("test", flag.ContinueOnError)
+	RegisterFlags(flagSet)
+	return flagSet
+}
+
+func Test_LoadConfig_Defaults(t *testing.T) {
+	t.Parallel()
+	require := require.New(t)
+
+	config, err := LoadConfig(&flag.FlagSet{})
+	require.NoError(err)
+
+	require.Equal(defaultSettings["log-level"], config.LogLevel)
+	require.Equal(defaultSettings["log-format"], config.LogFormat)
+	require.Equal(defaultSettings["graceful-shutdown-timeout"], config.GracefulShutdownTimeout)
+	require.Equal(defaultSettings["base-url"], config.BaseURL)
+	require.Equal(defaultSettings["port"], config.Port)
+	require.Equal(defaultSettings["interface"], config.Interface)
+	require.Equal(defaultSettings["go-collector"], config.GoCollector)
+	require.Equal(defaultSettings["process-collector"], config.ProcessCollector)
+	require.Equal(defaultSettings["calibre-db-path"], config.CalibreDBPath)
+
+	db := defaultSettings["db"].(map[string]interface{})
+	require.Equal(db["driver"], config.DB.Driver)
+	require.Equal(db["dsn"], config.DB.DSN)
+	require.Equal(db["max-idle-conns"], config.DB.MaxIdleConns)
+	require.Equal(db["max-open-conns"], config.DB.MaxOpenConns)
+	require.Equal(db["conn-max-lifetime"], config.DB.ConnMaxLifetime)
+
+	task := defaultSettings["task"].(map[string]interface{})
+	require.Equal(task["workers"], config.Task.Workers)
+	require.Equal(task["queue-length"], config.Task.QueueLength)
+	require.Equal(task["cadence"], config.Task.Cadence)
+
+	require.Equal(defaultSettings["jwt-issuer"], config.JWTIssuer)
+	require.Equal(defaultSettings["jwt-expiry"], config.JWTExpiry)
+
+	require.NotEmpty(config.JWTSecret)
+	firstSecret := config.JWTSecret
+
+	config2, err := LoadConfig(&flag.FlagSet{})
+	require.NoError(err)
+	require.NotEqual(firstSecret, config2.JWTSecret, "JWT Secret should be unique when not set")
+}
+
+func Test_LoadConfig_Flags(t *testing.T) {
+	t.Parallel()
+	require := require.New(t)
+	flags := testFlagSet()
+	flags.Set("log-level", "debug")
+	flags.Set("log-format", "json")
+	flags.Set("graceful-shutdown-timeout", "3s")
+	flags.Set("base-url", "http://localhost:7070")
+	flags.Set("go-collector", "false")
+	flags.Set("process-collector", "false")
+	flags.Set("port", "7070")
+	flags.Set("interface", "192.0.2.1") // "TEST-NET" in RFC 5737
+	flags.Set("calibre-db-path", "/tmp/calibre.db")
+	flags.Set("db.driver", "postgres")
+	flags.Set("db.dsn", "postgres://user:pass@localhost:5432/dbname?sslmode=disable")
+	flags.Set("db.max-idle-conns", "20")
+	flags.Set("db.max-open-conns", "21")
+	flags.Set("db.conn-max-lifetime", "2h")
+	flags.Set("task.workers", "15")
+	flags.Set("task.queue-length", "10")
+	flags.Set("task.cadence", "1s")
+	flags.Set("jwt-secret", "asdf")
+	flags.Set("jwt-issuer", "issuer")
+	flags.Set("jwt-expiry", "12h")
+
+	config, err := LoadConfig(flags)
+	require.NoError(err)
+
+	require.Equal("debug", config.LogLevel)
+	require.Equal("json", config.LogFormat)
+	require.Equal(3*time.Second, config.GracefulShutdownTimeout)
+	require.Equal("http://localhost:7070", config.BaseURL)
+	require.False(config.GoCollector)
+	require.False(config.ProcessCollector)
+	require.Equal(7070, config.Port)
+	require.Equal("/tmp/calibre.db", config.CalibreDBPath)
+	require.Equal("postgres", config.DB.Driver)
+	require.Equal("postgres://user:pass@localhost:5432/dbname?sslmode=disable", config.DB.DSN)
+	require.Equal(20, config.DB.MaxIdleConns)
+	require.Equal(21, config.DB.MaxOpenConns)
+	require.Equal(2*time.Hour, config.DB.ConnMaxLifetime)
+	require.Equal(15, config.Task.Workers)
+	require.Equal(10, config.Task.QueueLength)
+	require.Equal(1*time.Second, config.Task.Cadence)
+	require.Equal("asdf", config.JWTSecret)
+	require.Equal("issuer", config.JWTIssuer)
+	require.Equal(12*time.Hour, config.JWTExpiry)
+}
+
+func Test_LoadConfig_Env(t *testing.T) {
+	// Env cannot be set in parallel tests
+	require := require.New(t)
+
+	t.Setenv("LOG_LEVEL", "debug")
+	t.Setenv("LOG_FORMAT", "json")
+	t.Setenv("GRACEFUL_SHUTDOWN_TIMEOUT", "3s")
+	t.Setenv("BASE_URL", "http://localhost:7070")
+	t.Setenv("GO_COLLECTOR", "false")
+	t.Setenv("PROCESS_COLLECTOR", "false")
+	t.Setenv("PORT", "7070")
+	t.Setenv("INTERFACE", "192.0.2.1") // "TEST-NET" in RFC 5737
+	t.Setenv("CALIBRE_DB_PATH", "/tmp/calibre.db")
+	t.Setenv("DB__DRIVER", "postgres")
+	t.Setenv("DB__DSN", "postgres://user:pass@localhost:5432/dbname?sslmode=disable")
+	t.Setenv("DB__MAX_IDLE_CONNS", "20")
+	t.Setenv("DB__MAX_OPEN_CONNS", "21")
+	t.Setenv("DB__CONN_MAX_LIFETIME", "2h")
+	t.Setenv("TASK__WORKERS", "15")
+	t.Setenv("TASK__QUEUE_LENGTH", "10")
+	t.Setenv("TASK__CADENCE", "1s")
+	t.Setenv("JWT_SECRET", "asdf")
+	t.Setenv("JWT_ISSUER", "issuer")
+	t.Setenv("JWT_EXPIRY", "12h")
+
+	config, err := LoadConfig(&flag.FlagSet{})
+	require.NoError(err)
+
+	require.Equal("debug", config.LogLevel)
+	require.Equal("json", config.LogFormat)
+	require.Equal(3*time.Second, config.GracefulShutdownTimeout)
+	require.Equal("http://localhost:7070", config.BaseURL)
+	require.False(config.GoCollector)
+	require.False(config.ProcessCollector)
+	require.Equal(7070, config.Port)
+	require.Equal("/tmp/calibre.db", config.CalibreDBPath)
+	require.Equal("postgres", config.DB.Driver)
+	require.Equal("postgres://user:pass@localhost:5432/dbname?sslmode=disable", config.DB.DSN)
+	require.Equal(20, config.DB.MaxIdleConns)
+	require.Equal(21, config.DB.MaxOpenConns)
+	require.Equal(2*time.Hour, config.DB.ConnMaxLifetime)
+	require.Equal(15, config.Task.Workers)
+	require.Equal(10, config.Task.QueueLength)
+	require.Equal(1*time.Second, config.Task.Cadence)
+	require.Equal("asdf", config.JWTSecret)
+	require.Equal("issuer", config.JWTIssuer)
+	require.Equal(12*time.Hour, config.JWTExpiry)
+}
+
+func Test_LoadConfig_Env_Partials(t *testing.T) {
+	// env cannot be set in parallel tests
+	require := require.New(t)
+	flags := testFlagSet()
+	flags.Set("db.driver", "postgres")
+	flags.Set("db.dsn", "postgres://user:pass@localhost:5432/dbname?sslmode=disable")
+	flags.Set("db.max-idle-conns", "25")
+	flags.Set("log-level", "debug")
+
+	t.Setenv("DB__MAX_IDLE_CONNS", "20")
+	t.Setenv("LOG_FORMAT", "json")
+
+	config, err := LoadConfig(flags)
+	require.NoError(err)
+
+	require.Equal("postgres", config.DB.Driver, "flag should override default")
+	require.Equal("postgres://user:pass@localhost:5432/dbname?sslmode=disable", config.DB.DSN, "flag should override defatul")
+	require.Equal(25, config.DB.MaxIdleConns, "flag should override env and default")
+	require.Equal("debug", config.LogLevel, "flag should override default")
+	require.Equal("json", config.LogFormat, "env shoudl override default")
+	require.Equal(10, config.Task.Workers, "should use default value")
+}
+
+func Test_Validate(t *testing.T) {
+	tests := []struct {
+		name    string
+		config  Config
+		wantErr bool
+	}{
+		{
+			name: "valid",
+			config: Config{
+				LogLevel:                "debug",
+				LogFormat:               "json",
+				GracefulShutdownTimeout: 3 * time.Second,
+				BaseURL:                 "http://localhost:7070",
+				Port:                    7070,
+				Interface:               "192.0.2.1", // "TEST-NET" in RFC 5737
+				GoCollector:             false,
+				ProcessCollector:        false,
+				CalibreDBPath:           "/tmp/calibre.db",
+				DB:                      DatabaseConfig{Driver: "sqlite3", DSN: "file::memory:?cache=shared"},
+				Task:                    TaskConfig{Workers: 10, QueueLength: 100, Cadence: 5 * time.Second},
+				JWTSecret:               "asdf",
+				JWTIssuer:               "issuer",
+				JWTExpiry:               12 * time.Hour,
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid log level",
+			config: Config{
+				LogLevel:                "invalid",
+				LogFormat:               "json",
+				GracefulShutdownTimeout: 3 * time.Second,
+				BaseURL:                 "http://localhost:7070",
+				Port:                    7070,
+				Interface:               "192.0.2.1", // "TEST-NET" in RFC 5737
+				GoCollector:             false,
+				ProcessCollector:        false,
+				CalibreDBPath:           "/tmp/calibre.db",
+				DB:                      DatabaseConfig{Driver: "sqlite3", DSN: "file::memory:?cache=shared"},
+				Task:                    TaskConfig{Workers: 10, QueueLength: 100, Cadence: 5 * time.Second},
+				JWTSecret:               "asdf",
+				JWTIssuer:               "issuer",
+				JWTExpiry:               12 * time.Hour,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid log format",
+			config: Config{
+				LogLevel:                "debug",
+				LogFormat:               "invalid",
+				GracefulShutdownTimeout: 3 * time.Second,
+				BaseURL:                 "http://localhost:7070",
+				Port:                    7070,
+				Interface:               "192.0.2.1", // "TEST-NET" in RFC 5737
+				GoCollector:             false,
+				ProcessCollector:        false,
+				CalibreDBPath:           "/tmp/calibre.db",
+				DB:                      DatabaseConfig{Driver: "sqlite3", DSN: "file::memory:?cache=shared"},
+				Task:                    TaskConfig{Workers: 10, QueueLength: 100, Cadence: 5 * time.Second},
+				JWTSecret:               "asdf",
+				JWTIssuer:               "issuer",
+				JWTExpiry:               12 * time.Hour,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid db driver",
+			config: Config{
+				LogLevel:                "debug",
+				LogFormat:               "text",
+				GracefulShutdownTimeout: 3 * time.Second,
+				BaseURL:                 "http://localhost:7070",
+				Port:                    7070,
+				Interface:               "192.0.2.1", // "TEST-NET" in RFC 5737
+				GoCollector:             false,
+				ProcessCollector:        false,
+				CalibreDBPath:           "/tmp/calibre.db",
+				DB:                      DatabaseConfig{Driver: "invalid", DSN: "file::memory:?cache=shared"},
+				Task:                    TaskConfig{Workers: 10, QueueLength: 100, Cadence: 5 * time.Second},
+				JWTSecret:               "asdf",
+				JWTIssuer:               "issuer",
+				JWTExpiry:               12 * time.Hour,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid db dsn",
+			config: Config{
+				LogLevel:                "debug",
+				LogFormat:               "text",
+				GracefulShutdownTimeout: 3 * time.Second,
+				BaseURL:                 "http://localhost:7070",
+				Port:                    7070,
+				Interface:               "192.0.2.1", // "TEST-NET" in RFC 5737
+				GoCollector:             false,
+				ProcessCollector:        false,
+				CalibreDBPath:           "/tmp/calibre.db",
+				DB:                      DatabaseConfig{Driver: "invalid", DSN: ""},
+				Task:                    TaskConfig{Workers: 10, QueueLength: 100, Cadence: 5 * time.Second},
+				JWTSecret:               "asdf",
+				JWTIssuer:               "issuer",
+				JWTExpiry:               12 * time.Hour,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid task workers",
+			config: Config{
+				LogLevel:                "debug",
+				LogFormat:               "text",
+				GracefulShutdownTimeout: 3 * time.Second,
+				BaseURL:                 "http://localhost:7070",
+				Port:                    7070,
+				Interface:               "192.0.2.1", // "TEST-NET" in RFC 5737
+				GoCollector:             false,
+				ProcessCollector:        false,
+				CalibreDBPath:           "/tmp/calibre.db",
+				DB:                      DatabaseConfig{Driver: "invalid", DSN: ""},
+				Task:                    TaskConfig{Workers: -1, QueueLength: 100, Cadence: 5 * time.Second},
+				JWTSecret:               "asdf",
+				JWTIssuer:               "issuer",
+				JWTExpiry:               12 * time.Hour,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			require := require.New(t)
+			err := tt.config.Validate()
+			if tt.wantErr {
+				require.Error(err)
+			} else {
+				require.NoError(err)
+			}
+		})
+	}
+}
