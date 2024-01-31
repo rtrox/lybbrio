6b6624868a9ebb37dbb24dcd2a9d982941d879cb
Russell Troxel <russell.troxel@segment.com>
2024-01-31 12:44:59 -0800

    Initial Frontend Commit
    Signed-off-by: Russell Troxel <russell.troxel@segment.com>

---
+++ frontend/src/routes/about.lazy.tsx
@@ -0,0 +1,9 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+export const Route = createLazyFileRoute("/about")({
+  component: About,
+});
+
+function About() {
+  return <h3>Hello from About!</h3>;
+}
